b4c47617bd1500e688d5dcba629ed10f01bcc902
Dmytro Popovskyi <popovskiy96@gmail.com>
2024-09-23 18:55:28 +0100

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,26 @@
-
+def get_numbers_ticket(min: int, max: int, quantity: int):
+    min = int(min)
+    max = int(max)
+    quantity = int(quantity)
+    print(type(min))
+    if min >= max:
+        print('"min" value must be less than "max" value')
+        return
+    if quantity > (max - min):
+        print ('"quantity" must be less or equal max - min')
+        return
+    print(min is int)
+    result = generate_numbers_list(min, max)
+    return result
+    
+    
+def generate_numbers_list(min: int, max: int):
+    result_list = []
+    
+    while min <= max:
+        result_list.push(min)
+        min += 1
+    
+    return result_list
+    
+get_numbers_ticket(1, 1000, 50)
