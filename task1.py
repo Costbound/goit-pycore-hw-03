87f21d0c3ab4130a3b12a9c3539fcdad78f010d2
Dmytro Popovskyi <popovskiy96@gmail.com>
2024-09-23 11:24:29 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,7 +2,10 @@ from datetime import datetime
 
 def get_days_from_today(date):
     today = datetime.now()
-    selectedDate = datetime.fromisoformat(date)
-    return today.toordinal() - selectedDate.toordinal()
+    try:
+        selectedDate = datetime.fromisoformat(date)
+        return today.toordinal() - selectedDate.toordinal()
+    except ValueError:
+        print('Entered value must be a date in YYYY-MM-DD format')
     
-print(get_days_from_today('2024-09-21'))
+print(get_days_from_today('2024-10-02'))
