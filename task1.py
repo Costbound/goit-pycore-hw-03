e325284acfe21a976b5816fcd4826b4295ae0281
Dmytro Popovskyi <popovskiy96@gmail.com>
2024-09-23 10:07:18 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,8 @@
+from datetime import datetime
 
+def get_days_from_today(date):
+    today = datetime.now()
+    selectedDate = datetime.fromisoformat(date)
+    return today.toordinal() - selectedDate.toordinal()
+    
+print(get_days_from_today('2024-09-21'))
